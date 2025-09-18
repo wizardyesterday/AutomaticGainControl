e677b2b3027d1ce1e3d8c863a7383f5c79bfcab4
Chris G <chris@saturn.earth>
2025-09-18 09:37:46 -0500

    I decided to provide conversion from signal magnitude to decibels referenced to full scale.

---
+++ include/DbfsCalculator.h
@@ -0,0 +1,18 @@
+//**************************************************************************
+// file name: DbfsCalculator.h
+//**************************************************************************
+//_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+// This class implements a signal processing block that computes decibles
+// below a full scale value of a finite word length quantity.
+//_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+
+#ifndef __DBFSCALCULATOR__
+#define __DBFSCALCULATOR__
+
+#include <stdint.h>
+
+void dbfs_init(uint32_t wordLengthInBits);
+int32_t convertMagnitudeToDbFs(uint32_t signalMagnitude);
+
+
+#endif // __DBFSCALCULATOR__
